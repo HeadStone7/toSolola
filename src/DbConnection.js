bc0cc2c608ac90479c6b530c3b19a33f73590319
joe <joesharrit45@gmail.com>
2023-01-07 14:36:09 +0200

    blabla

--- src/DbConnection.js
+++ src/DbConnection.js
@@ -4,50 +4,27 @@ const mysql = require('mysql2');
 
 class DbConnection{
 
-    constructor(){
+    constructor() {
         this.pool = mysql.createPool({
-            host : 'localhost',
-            user: 'root',
-            password: '123456789',
-            database: 'chatapp',
-            connectionLimit: 10,
-            multipleStatements: true,
-            waitForConnections: true,
-            debug: false,
-            queueLimit: 0
-        })
-
-        this.connection = this.pool.promise() 
-
+                host: "localhost",
+                port: "3306",
+                user: "root",
+                password: "NNrWwpIQS1-1",
+                database: "mosungi",
+                waitForConnections: true,
+                connectionLimit: 10,
+                queueLimit: 0,
+                debug: false
+            }
+        )
+        this.connection = this.pool.promise();
     }
 
     async selQuery(){
-        const [res, zoe] = await this.connection.execute("SELECT * FROM contact")
+        const [res, zoe] = await this.connection.execute("SELECT * FROM users")
         console.log(zoe)
         console.log(res)
     }
 }
-
-// const connection = createPool({
-//     host : 'localhost',
-//     user: 'root',
-//     password: '123456789',
-//     database: 'chatapp',
-//     connectionLimit: 10,
-//     multipleStatements: true
-// });
-// console.log("connection..."+ connection)
-
-// // let row = function(){
-// connection.query('SELECT * FROM message', (err, rows) =>{
-//       if(err) throw err;
-//           console.log('Data received from Db:');
-//           console.log(rows);
-// })
-// }  
-
-// function connectionG(){
-//   return connection;
-// }
 
 module.exports = DbConnection
