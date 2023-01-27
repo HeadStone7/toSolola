00a5583c152739e61d3fb1f506bcb9e5308a10bf
HeadStone7omit <jos77marrion.com>
2023-01-28 04:21:46 +0800

    login page updated

--- src/ApiCall.js
+++ src/ApiCall.js
@@ -26,21 +26,46 @@ export class ApiCall {
 
         return resp.data
     }
-    // eslint-disable-next-line
-    async checkCredential(username, password){
-        let resp = await axios
-            .get('http://localhost:8080/test')
-            .catch(err => console.log("Error: ",err));
+    // eslint-disable-next-lin
+    async checkCredential(username, password) {
+        if (username === '' || password === '') {
+            return
+        } else {
+            let found = false
+            let resp = await axios
+                .get('http://localhost:8080/test')
+                .catch(err => console.log("Error: ", err));
 
-        for(let index in resp.data){
-            if(resp.data[index].username === username && resp.data[index].pwd === password){
-                console.log(username + " Connected")
-            }else {
-                console.log("Credential error")
+            for (let index in resp.data) {
+                if (resp.data[index].username === username && resp.data[index].pwd === password) {
+                    console.log(username + " Connected")
+                    found = true
+                } else {
+                    console.log(username+" Credential error")
+
+                }
             }
+
+            return found
         }
 
-        return resp.data
+        // if(username === '' || password === ''){
+        //     return;
+        // }else {
+        //     await axios
+        //         .get('http://localhost:8080/test')
+        //         .then(response =>{
+        //             for(let i = 0; i < response.data.length; i++){
+        //                 if(response.data[i].username === username && response.data[i].pwd === password){
+        //                     console.log(username+" connected");
+        //                     return true
+        //                 }
+        //             }
+        //             return false
+        //         })
+        //         .catch(err =>{
+        //             console.log('Error: '+err)
+        //         })
+        // }
     }
-
 }
