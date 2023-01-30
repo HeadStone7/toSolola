a5d7cce7e26177a8f016bf98e4a4ff8a8cda7e85
HeadStone7omit <jos77marrion.com>
2023-01-31 02:39:33 +0800

    contact.vue init

--- src/components/login/js/index.js
+++ src/components/login/js/index.js
@@ -1,55 +1,100 @@
 /* eslint-disable */
 import {ApiCall} from "../../../ApiCall";
-const registerPage = document.getElementById("registerPage");
-const loginPage = document.getElementById("loginPage");
-const loginTitleBtn = document.getElementById("loginState")
-const registerTitleBtn = document.getElementById("registerState")
-
 export default {
     components: {},
     data() {
-        return {}
+        return {
+            registerPage: null,
+            loginPage: null,
+            loginTitleBtn: null,
+            registerTitleBtn: null,
+            usernameLogin:null,
+            api: null,
+            showLogin: true,
+            showRegister: false,
+            showLoading: false
+        }
+    },
+    watch:{
     },
     beforeMount() {
-
+        this.api = new ApiCall()
     },
     mounted() {
+        this.registerPage = document.getElementById("registerPage");
+        this.loginPage = document.getElementById("loginPage");
+        this.loginTitleBtn = document.getElementById("loginState")
+        this.registerTitleBtn = document.getElementById("registerState")
     },
     methods: {
-        showHideLogin(){
-            loginPage.style.display = "flex";
-            registerPage.style.display = "none";
-            registerTitleBtn.style.backgroundColor = "";
-            loginTitleBtn.style.background = "#0e374f85";
+        showHideLogin: function () {
+            this.showLogin = true
+            this.showRegister = false
+            this.registerTitleBtn.style.backgroundColor = "";
+            this.loginTitleBtn.style.background = "#0e374f85";
         },
-        showHideRegister(){
-            registerPage.style.display = "flex"
-            registerPage.style.alignItems = "start"
-            loginPage.style.display = "none"
-            loginTitleBtn.style.backgroundColor = "#227bb0";
-            registerTitleBtn.style.backgroundColor = "#0e374f85";
+        showHideRegister: function () {
+            this.showLogin = false
+            this.showRegister = true
+            this.loginTitleBtn.style.backgroundColor = "#227bb0";
+            this.registerTitleBtn.style.backgroundColor = "#0e374f85";
         },
-        async loginFunc(){
-                const api = new ApiCall()
+        async loginFunc() {
+            let loginPhone = document.getElementById("loginPhoneInput").value;
+            let passwordLogin = document.getElementById("loginPasswordInput").value;
+            this.showLoading = true
 
-                let usernameLogin = document.getElementById("loginUsernameInput").value;
-                let passwordLogin = document.getElementById("loginPasswordInput").value;
-                console.log("loginFunc running ")
-                await api.checkCredential(usernameLogin, passwordLogin)
-                    .then(isValid =>{
-                        console.log('This is isValid'+isValid)
-                        if(isValid){
+            console.log("loginFunc running ")
+            setTimeout(async () => {
+                await this.api.checkCredential(loginPhone, passwordLogin)
+                    .then(isValid => {
+                        console.log('This is isValid = ' + isValid)
+                        if (isValid) {
                             this.$emit('loginState', true)
-                        }else {
-                            console.log(usernameLogin +"not connected")
+                        } else {
+                            console.log(loginPhone + " not connected")
                             this.$emit('loginState', false)
                         }
                     })
+                this.showLoading = false
+            }, 7000)
+        },
+        async registerFunc() {
+            let registerPhoneInput = document.getElementById("phoneRegisterInput").value
+            let registerUsernameInput = document.getElementById("usernameRegisterInput").value
+            let registerPassword = document.getElementById("passwordRegisterInput").value
+            let registerConfirmPwd = document.getElementById("confirmRegisterInput").value
+            this.showLoading = true
+            setTimeout(async () => {
+                await this.api.checkCredential(registerUsernameInput, registerPassword)
+                    .then(async isValid => {
+                        if (isValid) {
+                            console.log('Account already registered')
+                        } else {
+                            if (registerPassword !== registerConfirmPwd) {
+                                console.log('mot de passe non identique')
+                            } else {
+                                await this.api.register(registerUsernameInput, registerPassword, registerPhoneInput)
+                                    .then(isValid => {
+                                        console.log('in register isValid is = ' + isValid)
+                                        if (isValid) {
+                                            console.log('It\'s valide')
+                                            this.showHideLogin()
+                                        } else {
+                                            console.log('not valide')
+
+                                        }
+                                    })
+                                    .catch((err) => {
+                                        console.error(err);
+                                        return false
+                                    });
+                            }
+                        }
+                    })
+                this.showLoading = false
+            }, 7000)
 
         }
-
     }
 }
-
-
-
