d510f1572ad64bd92fbb06e69d8f7f2d9f41b1ef
HeadStone7omit <jos77marrion.com>
2023-02-03 20:18:46 +0800

    vuex store updated, City column added, stop loading when the server is offline.

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 
 import {createStore} from "vuex";
+import axios from "axios";
 
 const store = createStore({
     state:{
@@ -8,15 +9,39 @@ const store = createStore({
             userId:'',
             username:'',
             phone:'',
-            picture:''
+            picture:[],
+            city:''
+        },
+        networkStatus:{
+            state: false
         }
     },
     mutations:{
         updateUser(state, payload){
-            state.user.userId = payload.contact_id
+            state.user.userId = payload.user_id
             state.user.username = payload.username
             state.user.phone = payload.phone
-            state.user.picture = payload.profilePhoto
+            state.user.city = payload.city
+
+        },
+        /**
+         * listening to changes state of network to show/hide loagin/register
+         * show/hide loading when login/register
+         * @param status
+         * @param payload
+         */
+        networkUpdate(status, payload){
+            status.networkStatus.status = payload
+        },
+        setProfileImage(state, imageData) {
+            state.user.picture = imageData
+    }
+    },
+    actions:{
+        async fetchImage({commit}, userId){
+            const response = await axios.get(`http://localhost:8080/tosolola/api/get-profile-image?userId=${userId}`, {responseType: "blob"})
+            const imageData = new Uint8Array(response.data)
+            commit('setProfileImage', imageData)
         }
     }
 })
