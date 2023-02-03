55ee3675a76b679594095f21beaf81ac50de8764
HeadStone7omit <jos77marrion.com>
2023-02-03 19:27:49 +0800

    contact.vue component styled

--- src/views/contacts/contact.vue
+++ src/views/contacts/contact.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1516cff948266a6243739564b8f11c151840d038b68791384bad8fe6b30bd25
-size 405
+oid sha256:edb40b25a47c728fae130418e66f03e94f981a95dc87249cb10fed92b6ab76a7
+size 439
