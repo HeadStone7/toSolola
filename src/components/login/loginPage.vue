f646f11374e80830047944c8abbda61e1ad5bd64
HeadStone7omit <jos77marrion.com>
2023-02-03 20:13:11 +0800

    changing my loading boolean executor in template, updated by storing it in store vuex

--- src/components/login/loginPage.vue
+++ src/components/login/loginPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:308fdaaac189dfa26b2e3a55ba45b6d28b166b61a798ce70985e928c13012b54
-size 2062
+oid sha256:dea839116666d4bcfe9aead6549f0ff269512ce138c625b48c32923f1973950d
+size 2089
