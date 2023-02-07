a5498f096cc4f5dd5059708f45e4052b4c8210c4
HeadStone7omit <jos77marrion.com>
2023-02-07 15:05:57 +0800

    chat component style updated

--- src/components/Chat/ChatContactCompo.vue
+++ src/components/Chat/ChatContactCompo.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed82df6b033b29fbdbe97c27a9ca3d71fae24e51ea1a135ae872769089267fa2
-size 15638
+oid sha256:f827ed548c07db42fa3536984cf8296fe95a3c51c702d70ee34bdd9c4ab35075
+size 15711
